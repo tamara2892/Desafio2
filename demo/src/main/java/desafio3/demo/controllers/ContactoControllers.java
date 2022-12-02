bc6d63de939144baa4a1b6fe93dc163ef87661ae
tamara2892 <68615417+tamara2892@users.noreply.github.com>
2022-12-02 12:33:36 -0300

    Add files via upload

---
+++ demo/src/main/java/desafio3/demo/controllers/ContactoControllers.java
@@ -0,0 +1,35 @@
+package desafio3.demo.controllers;
+
+import desafio3.demo.domain.ContactoDTO;
+import desafio3.demo.domain.MensajeDTO;
+import desafio3.demo.service.ContactoService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/contacto")
+public class ContactoControllers {
+    private final ContactoService contactoService;
+
+    public ContactoControllers(ContactoService contactoService)  {
+        this.contactoService = contactoService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ContactoDTO>> findAll() {
+        return ResponseEntity.ok(contactoService.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<MensajeDTO> add(@RequestBody ContactoDTO contactoDTO) {
+        return ResponseEntity.ok(contactoService.add(contactoDTO));
+    }
+
+    @GetMapping("/{contactoid}")
+    public ResponseEntity<ContactoDTO> findById(@PathVariable Integer contactoid){
+        return ResponseEntity.ok(contactoService.findById(contactoid));
+    }
+
+}
